04868a5aa58ef16ec51d9fbc668590a2da2139a3
elmaxiboy <maxescovi@gmail.com>
2022-11-09 22:35:44 -0300

    test with local json file

--- scripts.js
+++ scripts.js
@@ -29,19 +29,34 @@ function showButtonDiv(className, id){
             fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCzgOgt27CN7sE6LqK-bA89w&maxResults=3&order=date&key=AIzaSyAOroVaBFLReOQ9SfUbHfY6EChLvRSCCCE")
             .then((results) => {return results.json()})
             .then((data)=> {
-                let videos =data.items;
-                
-                videos.forEach(video => {
 
+                const interestingStuffContainer=document.querySelector(".interesting-stuff");
+                
+
+                if(data.error){
+
+                    interestingStuffContainer.innerHTML+= '<div class="fakeimg" > Error '+data.error.code+" : "+data.error.message+' </div>'
+
+                
+                    fetch('./youtube_static_list.json')
+                    .then((response) => {return response.json() })
+                    .then((json) => console.log(json));
+
+                }
+
+                else{
+
+                    let videos =data.items;
+                    videos.forEach(video => {
                     let videoThumbnail = video.snippet.thumbnails.high.url;
                     let videoUrl= "https://www.youtube.com/watch?v="+video.id.videoId ;
-                    let videoTitle = video.snippet.title
-                    interestingStuffContainer=document.querySelector(".interesting-stuff");
+                    let videoTitle = video.snippet.title                  
                     interestingStuffContainer.innerHTML+= '<div class="fakeimg" > <a href='+videoUrl+' target="_blank" rel="noopener noreferrer"> <img class= "youtube-thumbnail" src='+videoThumbnail+' ></a>' +videoTitle+ '</div><br>'
-
-                });
-            })
-        
-        }
+                    }
+                    )
+                }
+            }  
+            );
+            }
 
  getLastYoutubeVideo()       
