190a1b16686ba54fa7abf37dbca5eaadeea5d253
vn51d7p <vn51d7p@walmart.com>
2022-11-10 10:34:38 -0300

    shuffled youtube thumbnails

--- scripts.js
+++ scripts.js
@@ -26,7 +26,7 @@ function showButtonDiv(className, id){
   }
 
   function getLastYoutubeVideo(){
-            fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCzgOgt27CN7sE6LqK-bA89w&maxResults=10&order=date&key=AIzaSyAOroVaBFLReOQ9SfUbHfY6EChLvRSCCCE")
+            fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCzgOgt27CN7sE6LqK-bA89w&maxResults=50&order=searchSortUnspecified&key=AIzaSyAOroVaBFLReOQ9SfUbHfY6EChLvRSCCCE")
             .then((results) => {return results.json()})
             .then((data)=> {
 
@@ -36,29 +36,53 @@ function showButtonDiv(className, id){
 
                 if(data.error){
 
-                    interestingStuffContainer.innerHTML+= '<div class="fakeimg" > Error '+data.error.code+" : "+data.error.message+' </div>'
-
-                
-                    fetch('./youtube_static_list.json')
-                    .then((response) => {return response.json() })
-                    .then((json) => console.log(json));
+                    interestingStuffContainer.innerHTML+= '<div class="fakeimg" > Error '+data.error.code+" while requesting video list via Youtube API : "+data.error.message+' </div>'
 
                 }
 
                 else{
 
                     let videos =data.items;
-                    videos.forEach(video => {
-                    let videoThumbnail = video.snippet.thumbnails.high.url;
-                    let videoUrl= "https://www.youtube.com/watch?v="+video.id.videoId ;
-                    let videoTitle = video.snippet.title                  
-                    interestingStuffContainer.innerHTML+= '<div class="fakeimg" > <a href='+videoUrl+' target="_blank" rel="noopener noreferrer"> <img class= "youtube-thumbnail" src='+videoThumbnail+' ></a>' +videoTitle+ '</div><br>'
+                    for (var a=[],i=0;i<videos.length;++i) a[i]=i;
+                    const shuffledArray=shuffle(a);
+
+                    maxVideos=3
+                    count=0
+                    shuffledArray.forEach(index => {
+
+                        if (videos[index].id.kind!="youtube#video"){
+                            //do nothing
+                        }
+                        else if(count==maxVideos){
+                            //do nothing
+                        }
+                        else{
+
+                            let videoThumbnail = videos[index].snippet.thumbnails.high.url;
+                            let videoUrl= "https://www.youtube.com/watch?v="+videos[index].id.videoId ;
+                            let videoTitle = videos[index].snippet.title                  
+                            interestingStuffContainer.innerHTML+= '<div class="fakeimg" > <a href='+videoUrl+' target="_blank" rel="noopener noreferrer"> <img class= "youtube-thumbnail" src='+videoThumbnail+' ></a>' +videoTitle+ '</div><br>'
+                            count+=1
+
+                        }
                     }
                     )
                 }
             }  
             );
             }
+
+            function shuffle(array) {
+                var tmp, current, top = array.length;
+                if(top) while(--top) {
+                  current = Math.floor(Math.random() * (top + 1));
+                  tmp = array[current];
+                  array[current] = array[top];
+                  array[top] = tmp;
+                }
+                return array;
+              }
+              
  getLastYoutubeVideo()     
  
  
