6bf662e24a4f3a69de08c8697786fce261bc6d30
vn51d7p <vn51d7p@walmart.com>
2022-11-10 08:51:45 -0300

    apply randomnes to thmbnail picking

--- scripts.js
+++ scripts.js
@@ -26,22 +26,39 @@ function showButtonDiv(className, id){
   }
 
   function getLastYoutubeVideo(){
-            fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCzgOgt27CN7sE6LqK-bA89w&maxResults=3&order=date&key=AIzaSyAOroVaBFLReOQ9SfUbHfY6EChLvRSCCCE")
+            fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCzgOgt27CN7sE6LqK-bA89w&maxResults=10&order=date&key=AIzaSyAOroVaBFLReOQ9SfUbHfY6EChLvRSCCCE")
             .then((results) => {return results.json()})
             .then((data)=> {
                 let videos =data.items;
+                let shouldSkip= false;
+                let counter=0;
                 
                 videos.forEach(video => {
+                    let randomBinary = Math.round(Math.random());
 
-                    let videoThumbnail = video.snippet.thumbnails.high.url;
-                    let videoUrl= "https://www.youtube.com/watch?v="+video.id.videoId ;
-                    let videoTitle = video.snippet.title
-                    interestingStuffContainer=document.querySelector(".interesting-stuff");
-                    interestingStuffContainer.innerHTML+= '<div class="fakeimg" > <a href='+videoUrl+' target="_blank" rel="noopener noreferrer"> <img class= "youtube-thumbnail" src='+videoThumbnail+' ></a>' +videoTitle+ '</div><br>'
 
+                    if (counter>=3){
+                        return
+                    }
+
+                    else if (randomBinary===1){
+                        let videoThumbnail = video.snippet.thumbnails.high.url;
+                        let videoUrl= "https://www.youtube.com/watch?v="+video.id.videoId ;
+                        let videoTitle = video.snippet.title
+                        interestingStuffContainer=document.querySelector(".interesting-stuff");
+                        interestingStuffContainer.innerHTML+= '<div class="fakeimg" > <a href='+videoUrl+' target="_blank" rel="noopener noreferrer"> <img class= "youtube-thumbnail" src='+videoThumbnail+' ></a>' +videoTitle+ '</div><br>'
+                        counter+=1;
+                    }
+
+                    else{
+
+                    }
+                    
                 });
             })
         
         }
 
- getLastYoutubeVideo()       
+ getLastYoutubeVideo()     
+ 
+ 
